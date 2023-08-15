795ce0463b19c3dc72449ee8ba2afce78a8b5d4f
Will <william.mendez@cuemby.com>
2023-08-14 22:54:53 -0600

    feat: checking the error

--- .github/workflows/scripts/print.sh
+++ .github/workflows/scripts/print.sh
@@ -4,6 +4,7 @@
 # Exit if any command fails
 
 set -e
+echo "❌::error title='Coverage of does not meet the required threshold of coverage.'❌"
 
 echo "GH_REPO: $GH_REPO"
 echo "GH_REPO_O: $GH_REPO_O"
